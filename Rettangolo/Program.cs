30dad5500524742ca88a68fb3ef1332f14b049b5
atadd <atadd@DESKTOP-PVF3S8C>
2024-01-24 17:34:55 +0100

    Aggiungere i file di progetto.

---
+++ Rettangolo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rettangolo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rettangolo rettangolo = new Rettangolo();
+            Console.WriteLine(" ");
+            rettangolo.latominore=double.Parse(Console.ReadLine());
+            Console.WriteLine(" ");
+            rettangolo.latomaggiore = double.Parse(Console.ReadLine());
+            Console.WriteLine("{0}",rettangolo.area());
+            Console.WriteLine("{0}", rettangolo.perimetro());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Rettangolo/Rettangolo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rettangolo
+{
+    internal class Rettangolo
+    {
+        public double latomaggiore { get; set; }
+        public double latominore { get; set; }
+        public double area()
+        {
+            double area;
+            area = latomaggiore * latominore;
+            return area;
+        }
+        public double perimetro()
+        {
+            double perimetro;
+            perimetro= latomaggiore*2+latominore*2;
+            return perimetro;
+        }
+    }
+}
